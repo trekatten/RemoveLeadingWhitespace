bccb81b126aa4fe33587672b3c3f1a75135494ff
trekatten <trekatten@gmail.com>
2019-03-10 22:28:20 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from aqt import mw
+from anki.hooks import addHook
+import re
+
+# Of course anki uses &nbsp instead of " " for spaces in the fields
+regex = re.compile(r"(&nbsp;){1,}")
+
+def onFocusLost(flag, n, fidx):
+    # does the note have a 'meaning' field?
+    for c, name in enumerate(mw.col.models.fieldNames(n.model())):
+        if name.lower() == "meaning":
+            src = name
+            fieldIndex = c
+    if not fieldIndex:
+        return flag
+
+    # is the event comming from the "meaning" field?
+    if fidx != fieldIndex:
+        return flag
+
+    # grab the text and remove leading spaces
+    n[src] = regex.sub("", n[src])
+    return True
+
+addHook('editFocusLost', onFocusLost)
